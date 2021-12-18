d1807ab36d869d379727b2ece0a19a4aa19f772d
Ipung Anwar <ipung.anwar1@gmail.com>
2021-12-19 02:45:53 +0700

    Add files via upload

---
+++ pages/blog/_slug.vue
@@ -0,0 +1,89 @@
+<template>
+  <article class="pt-24 p-24 grid grid-cols-12">
+    <!-- <pre> {{ article }} </pre> -->
+
+    <nuxt-img
+      class="rounded-lg col-start-3 col-span-8 w-full"
+      :src="article.img"
+      width="768"
+      height="509"
+    />
+
+    <div class="w-full col-start-1 col-span-12 grid grid-cols-12">
+      <nav class="mt-8 col-span-2 shadow-2xl rounded-lg p-4 self-start">
+        <ul class="space-y-2">
+          <li v-for="link of article.toc" :key="link.id">
+            <NuxtLink
+              class="uppercase tracking-wider text-gray-500 hover:underline"
+              :to="`#${link.id}`"
+              >{{ link.text }}</NuxtLink>
+            >
+          </li>
+        </ul>
+      </nav>
+      <div class="col-start-4 col-span-6 w-full">
+        <div>
+          <NuxtLink to="/" href="/" class="block text-gray-400 mt-8">← Go Back</NuxtLink>
+          <ul v-if="article.tags" class="flex space-x-3 mt-2">
+            <li
+              class="text-gray-400 font-bold"
+              v-for="tag in article.tags"
+              :key="tag"
+            >
+              {{ tag }}
+            </li>
+          </ul>
+          <h1 class="text-5xl font-black mt-2">{{ article.title }}</h1>
+        </div>
+        <nuxt-content class="mt-4 prose max-w-none" :document="article" />
+      </div>
+    </div>
+  </article>
+</template>
+
+<script>
+  export default {
+    async asyncData({ $content, params }) {
+      const article = await $content('articles', params.slug).fetch()
+
+      const [prev, next] = await $content('articles')
+      .only(['title', 'slug'])
+      .sortBy('createdAt', 'asc')
+      .surround(params.slug)
+      .fetch()
+
+      return {
+        article,
+        prev,
+        next,
+      }
+    },
+    methods: {
+      formatDate(date) {
+        const options = { year: 'numeric', month: 'long', day: 'numeric' }
+        return new Date(date).toLocaleDateString('en', options)
+      }
+    }
+  }
+</script>
+
+<style>
+  .nuxt-content h2 {
+    font-weight: bold;
+    font-size: 28px;
+  }
+  .nuxt-content h3 {
+    font-weight: bold;
+    font-size: 22px;
+  }
+  .nuxt-content p {
+    margin-bottom: 20px;
+  }
+  .icon.icon-link {
+    background-image: url('~assets/svg/icon-hashtag.svg');
+    display: inline-block;
+    width: 20px;
+    height: 20px;
+    background-size: 20px 20px;
+  }
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="pt-24 p-24">
+    <h1 class="text-7xl font-black text-center">New on the blog</h1>
+    <ul class="grid grid-cols-3 gap-8 mt-8">
+      <ArticlePreview v-for="article in articles" :key="article.slug" :article="article"></ArticlePreview>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData({ $content, params }) {
+    const articles = await $content('articles', params.slug).fetch()
+
+    return { articles }
+  },
+}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+</style>
