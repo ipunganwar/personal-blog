25efaf1ed88169680d45d48d22be4857f6512a44
Ipung Anwar <ipung.anwar1@gmail.com>
2021-12-19 02:14:17 +0700

    add config nuxt

---
+++ components/ArticlePreview.vue
@@ -0,0 +1,27 @@
+<template>
+  <li class="transition-all duration-500 ease-in-out">
+    <a :href="`/blog/${article.slug}`">
+      <nuxt-img
+        class="rounded-lg"
+        :src="article.img"
+        preset="preview"
+        width="400"
+        height="300"
+      />
+      <ul v-if="article.tags" class="flex space-x-3 mt-2">
+        <li class="text-gray-400 font-bold" v-for="tag in article.tags" :key="tag">
+          {{ tag }}
+        </li>
+      </ul>
+      <h2 class="mt-2 text-3xl font-semibold">{{ article.title }}</h2>
+    </a>
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    article: Object,
+  }
+}
+</script>
---
+++ nuxt.config.js
@@ -0,0 +1,78 @@
+export default {
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  target: 'static',
+  head: {
+    title: 'ipung-anwar',
+    htmlAttrs: {
+      lang: 'en'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  content: {
+    markdown: {
+      prism: {
+        theme: 'prism-themes/themes/prism-material-oceanic.css'
+      }
+    }
+  },
+  image: {
+    screens: {
+      xs: 320,
+      sm: 640,
+      md: 768,
+      lg: 1024,
+      xl: 1280,
+      xxl: 1536,
+      '2xl': 1536,
+    },
+    presets: {
+      preview: {
+        modifiers: {
+          fit: 'cover',
+          format: 'jpg',
+          width: 400,
+          height: 300,
+        },
+      },
+    },
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    '@nuxtjs/tailwindcss',
+    '@nuxt/image',
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    '@nuxt/content'
+  ],
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+    postcss: {
+      plugins: {
+        tailwindcss: {},
+        autoprefixer: {},
+      },
+    },
+  }
+}
